cce66f37203124264f97145953ee9bed50a43307
Rahul Bansod <rahulbansod519@gmail.com>
2025-11-23 09:26:32 +0530

    build: Add Dockerfile, update docker-compose postgres port mapping, and set Java version to 17.

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerRepository.java
@@ -17,14 +17,15 @@ package org.springframework.samples.petclinic.owner;
 
 import java.util.Optional;
 
-import jakarta.annotation.Nonnull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Repository class for <code>Owner</code> domain objects. All method names are compliant
- * with Spring Data naming conventions so this interface can easily be extended for Spring
+ * Repository class for <code>Owner</code> domain objects. All method names are
+ * compliant
+ * with Spring Data naming conventions so this interface can easily be extended
+ * for Spring
  * Data. See:
  * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.query-creation
  *
@@ -37,26 +38,33 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
 	/**
-	 * Retrieve {@link Owner}s from the data store by last name, returning all owners
+	 * Retrieve {@link Owner}s from the data store by last name, returning all
+	 * owners
 	 * whose last name <i>starts</i> with the given name.
+	 * 
 	 * @param lastName Value to search for
-	 * @return a Collection of matching {@link Owner}s (or an empty Collection if none
-	 * found)
+	 * @return a Collection of matching {@link Owner}s (or an empty Collection if
+	 *         none
+	 *         found)
 	 */
 	Page<Owner> findByLastNameStartingWith(String lastName, Pageable pageable);
 
 	/**
 	 * Retrieve an {@link Owner} from the data store by id.
 	 * <p>
-	 * This method returns an {@link Optional} containing the {@link Owner} if found. If
+	 * This method returns an {@link Optional} containing the {@link Owner} if
+	 * found. If
 	 * no {@link Owner} is found with the provided id, it will return an empty
 	 * {@link Optional}.
 	 * </p>
+	 * 
 	 * @param id the id to search for
-	 * @return an {@link Optional} containing the {@link Owner} if found, or an empty
-	 * {@link Optional} if not found.
-	 * @throws IllegalArgumentException if the id is null (assuming null is not a valid
-	 * input for id)
+	 * @return an {@link Optional} containing the {@link Owner} if found, or an
+	 *         empty
+	 *         {@link Optional} if not found.
+	 * @throws IllegalArgumentException if the id is null (assuming null is not a
+	 *                                  valid
+	 *                                  input for id)
 	 */
 	Optional<Owner> findById(Integer id);
 
